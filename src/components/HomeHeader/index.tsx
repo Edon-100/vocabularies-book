46d56a1b440c25b7da6c13bc559a01aaf9c0a5e0
Edon <laixiaodong@codemao.cn>
2021-09-29 16:30:25 +0800

    feat: 增加单词列表操作说明

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.less'
 
-export const HomeHeader = (props) => {
+export const HomeHeader = (props:any) => {
   console.log(props)
   return (
     <div className="home-header-wrapper">
